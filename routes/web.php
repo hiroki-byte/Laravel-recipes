a34ddd6c00a8d532d68fc49e83a18819f331959a
hiroki-byte <142847101+hiroki-byte@users.noreply.github.com>
2023-08-23 18:10:30 +0900

    Revert "アカウントをロックする機能及びコントローラのスリム化"

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,5 @@ Route::get('/', function(){
 
 Route::get('/show_all', [RecipesController::class, 'showAll']);
 Route::resource('recipes', RecipesController::class);
-
-Route::middleware(['guest'])->group(function () {
-    Route::get('/', [AuthController::class,'showLogin'])->name('login.show');
-    Route::post('/login',[AuthController::class, 'Login'])->name('login');
-});
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('home', function() {
-        return view('home');
-    })->name('home');
-
-    Route::post('logout',[AuthController::class, 'logout'])->name('logout');
-});
-
-
+Route::get('/', [AuthController::class,'showLogin'])->name('showLogin');
+Route::post('/login',[AuthController::class, 'login'])->name('login');
