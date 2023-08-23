00df5a5995f23c899dc697d1f5b35ec8761c176e
frey <hongjidubu@gmail.com>
2023-08-23 18:08:38 +0900

    fat model

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -10,6 +10,10 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
     /**
     * @return View
     */
@@ -25,26 +29,20 @@ class AuthController extends Controller
     public function Login(LoginFormRequest $request)
     {
         $credentials = $request->only('email','password');
-        $user = User::where('email', "=", $credentials['email'])->first();
+        $user = $this->user->getUserByEmail($credentials['email']);
         if(!is_null($user)){
-            if($user->locked_flg === 1){
+            if($this->user->isAccountLocked($user)){
                 return back()->withErrors([
                     'login_error' => 'アカウントがロックされています。',
                 ]);
             }
             if(Auth::attempt($credentials)){
                 $request->session()->regenerate();
-                if($user->error_count > 0){
-                    $user->error_count = 0;
-                    $user->save();
-                }
+                $this->user->resetErrorCount($user);
                 return redirect()->route('home')->with('login_success','ログイン成功しました！');
             }
-            $user->error_count = $user->error_count + 1;
-            if($user->error_count > 5){
-                $user->locked_flg = 1;
-                $user->save();
-
+            $user->error_count = $this->user->addErrorCount($user->error_count);
+            if($this->user->lockAccount($user)){
                 return back()->withErrors([
                     'login_error' => 'アカウントがロックされました。',
                 ]);
