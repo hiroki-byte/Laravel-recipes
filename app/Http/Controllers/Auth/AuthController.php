e1a3d712a91a243c35926ad100453ed10df2beb2
frey <hongjidubu@gmail.com>
2023-08-23 17:09:13 +0900

    create account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -23,17 +24,37 @@ class AuthController extends Controller
     */
     public function Login(LoginFormRequest $request)
     {
-       $credentials = $request->only('email','password');
+        $credentials = $request->only('email','password');
+        $user = User::where('email', "=", $credentials['email'])->first();
+        if(!is_null($user)){
+            if($user->locked_flg === 1){
+                return back()->withErrors([
+                    'login_error' => 'アカウントがロックされています。',
+                ]);
+            }
+            if(Auth::attempt($credentials)){
+                $request->session()->regenerate();
+                if($user->error_count > 0){
+                    $user->error_count = 0;
+                    $user->save();
+                }
+                return redirect()->route('home')->with('login_success','ログイン成功しました！');
+            }
+            $user->error_count = $user->error_count + 1;
+            if($user->error_count > 5){
+                $user->locked_flg = 1;
+                $user->save();
 
-       if(Auth::attempt($credentials)){
-        $request->session()->regenerate();
+                return back()->withErrors([
+                    'login_error' => 'アカウントがロックされました。',
+                ]);
+            }
+            $user->save();
+        }
 
-        return redirect()->route('home')->with('login_success','ログイン成功しました！');
-       }
-
-       return back()->withErrors([
-        'login_error' => 'メールアドレスかパスワードが間違っています。',
-       ]);
+        return back()->withErrors([
+            'login_error' => 'メールアドレスかパスワードが間違っています。',
+        ]);
     }
      /**
      * ユーザーをアプリケーションからログアウトさせる
